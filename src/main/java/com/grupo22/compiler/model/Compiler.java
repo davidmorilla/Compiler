139f380b1b35811b7a2b980d5212e77b618a95da
David Morilla <165312567+davidmorilla@users.noreply.github.com>
2024-06-18 22:37:16 +0100

    Errores lexicos

--- src/main/java/com/grupo22/compiler/model/Compiler.java
+++ src/main/java/com/grupo22/compiler/model/Compiler.java
@@ -38,7 +38,7 @@ public class Compiler {
 	public final static String TOKENS_OUTPUT_FORMAT = "src/main/java/com/grupo22/compiler/output/tokens_output%d.txt";
 	public final static String PARSE_OUTPUT_FORMAT = "src/main/java/com/grupo22/compiler/output/parse_output%d.txt";
 
-	final static int CODE_FILE_NUMBER = 19; //Cambiar aquí el numero de codigo de ejemplo a parsear
+	final static int CODE_FILE_NUMBER = 1; //Cambiar aquí el numero de codigo de ejemplo a parsear
 
 	public static void main (String args[]) {
 		String CODE_FILE_NAME = String.format(CODE_FILE_NAME_FORMAT, CODE_FILE_NUMBER);
@@ -285,8 +285,10 @@ public class Compiler {
 				System.out.println("entra 278");
 				TSControl.putSimbolo(palabra);				
 			}
-			else if((!TSControl.isGlobal()&&TSControl.existeLex(palabra)==1&&declaracionExplicita)||(TSControl.isGlobal()&&TSControl.existeLex(palabra)==0&&declaracionExplicita)||(TSControl.isGlobal()&&TSControl.existeLex(palabra)==0&&!declaracionExplicita)){
+			else if((!TSControl.isGlobal()&&TSControl.existeLex(palabra)==1&&declaracionExplicita)||(TSControl.isGlobal()&&TSControl.existeLex(palabra)==0&&declaracionExplicita)){
 			//else {
+				System.out.println("entra 291");
+				System.out.println(TSControl.isGlobal() + (TSControl.existeLex(palabra) + palabra + declaracionExplicita ));
 			genError(17, line, palabra);
 			}
 			
